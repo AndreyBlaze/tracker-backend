c94c35b19fd8d0795b1173a5429f30d634fe6a82
andrey <feand20041102@gmail.com>
2024-04-24 20:43:02 +0300

    up

---
+++ Infrastructure/Persistence/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public sealed class ApplicationContext : DbContext, IUnitOfWork
+{
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+}
